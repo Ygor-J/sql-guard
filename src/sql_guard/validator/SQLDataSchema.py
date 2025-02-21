1ca73c74857c6458171d427e806cdc69bb8eb79d
Ygor-J <ygor-skat@hotmail.com>
2025-02-21 17:28:02 -0300

    Create basic checks

--- src/sql_guard/validator/SQLDataSchema.py
+++ src/sql_guard/validator/SQLDataSchema.py
@@ -1,25 +1,36 @@
 from typing import Dict, List, Optional
+from dataclasses import dataclass
 
-
+@dataclass
 class ValidationCheck:
-    pass
+    '''Base class for validation checks'''
+    check_name: str
+    params: dict
+    error_msg: Optional[str] = None
 
 class SQLValidator:
 
-    def __init__(self, data_constraints: Dict[str, List[ValidationCheck]]):
+    def __init__(self, data_rules: Dict[str, List[ValidationCheck]], sql_dialect: str = "GoogleSQL"):
         '''
         Create a SQLValidator object that can create SQL validations based on data quality rules.
 
         :param data_rules: Dictionary of data quality rules
+        :param sql_dialect: SQL Dialect of SQL query
 
         '''
-        self.data_rules = data_constraints
+        self.data_rules = data_rules
+        self.sql_dialect = sql_dialect
+        self._validate_schema()
 
-    def __str__(self) -> str:
-        return self.data_rules
+    def _validate_schema(self):
+        '''Pre-validating schema passed into SQL Validator class'''
+        for column, checks in self.data_rules.items():
+            if not isinstance(checks, list):
+                raise ValueError(f"Invalid data constraints for column {column}")
+            for check in checks:
+                if not isinstance(check, ValidationCheck):
+                    raise ValueError(f"Invalid ValidationCheck for column {column}")
 
-    def __repr__(self) -> str:
-        return f"SQLDataSchema({self.data_rules})" 
 
     def to_sql(self, from_source: str='your_table') -> str:
 
@@ -29,13 +40,35 @@ class SQLValidator:
         for column, rules in self.data_rules.items():
             column_conditions = list()
             for rule in rules:
-                rule, _, value = rule.partition(':')
-                if rule == 'is_integer':
+                check_name = rule.check_name
+                if check_name == 'is_integer':
                     column_conditions.append(f'SAFE_CAST({column} AS INT) IS NULL')
-                elif rule == 'greater_than':
+                if check_name == 'is_string':
+                    column_conditions.append(f'SAFE_CAST({column} AS STRING) IS NULL')
+                if check_name == 'is_boolean':
+                    column_conditions.append(f'SAFE_CAST({column} AS BOOL) IS NULL')
+                if check_name == 'is_float':
+                    column_conditions.append(f'SAFE_CAST({column} AS FLOAT) IS NULL')
+                if check_name == 'is_date':
+                    column_conditions.append(f'SAFE_CAST({column} AS DATE) IS NULL')
+                if check_name == 'is_timestamp':
+                    column_conditions.append(f'SAFE_CAST({column} AS TIMESTAMP) IS NULL')
+                elif check_name == 'greater_than':
+                    value = rule.params['value']
                     column_conditions.append(f"{column} <= {value}")
-                elif rule == 'starts_with':
+                elif check_name == 'starts_with':
+                    value = rule.params['value']
                     column_conditions.append(f"{column} NOT LIKE '{value}%'")
+                elif check_name == 'ends_with':
+                    value = rule.params['value']
+                    column_conditions.append(f"{column} NOT LIKE '%{value}'")
+                elif check_name == 'str_contains':
+                    value = rule.params['value']
+                    column_conditions.append(f"{column} NOT LIKE '%{value}%'")
+                elif check_name == 'regex_contains':
+                    value = rule.params['value']
+                    column_conditions.append(f"NOT REGEXP_CONTAINS({column}, '{value}'")
+
             if column_conditions:
                 val_conditions.append(' OR '.join(column_conditions))
                 invalid_values.append(f"IFNULL(ARRAY_AGG(IF({' OR '.join(column_conditions)}, SAFE_CAST({column} AS STRING), NULL) IGNORE NULLS), [])")
@@ -64,11 +97,27 @@ class SQLValidator:
     
 
 rules = {
-    'column_a': ['is_integer'],
-    'column_b': ['is_str', 'starts_with:a'],
-    'column_c': ['is_integer', 'greater_than:0']
+    "age": [
+        ValidationCheck(
+            check_name="is_integer",
+            params={},
+            error_msg="Age must be an integer"
+        ),
+        ValidationCheck(
+            check_name="greater_than",
+            params={"value": 0},
+            error_msg="Age must be positive"
+        )
+    ],
+    "email": [
+        ValidationCheck(
+            check_name="starts_with",
+            params={"value": "user_"},
+            error_msg="Invalid email format"
+        )
+    ]
 }
 
-myTable = SQLDataSchema(rules)
+myTableSChema = SQLValidator(rules)
 
-print(myTable.to_sql())
+print(myTableSChema.to_sql())
