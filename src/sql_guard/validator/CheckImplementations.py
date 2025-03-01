b75b4ba816c798ad2c6b5196c5a7db516b7a2be6
Ygor-J <ygor-skat@hotmail.com>
2025-03-01 14:48:46 -0300

    Finish basic structure of method to_sql of class SQLValidator

--- src/sql_guard/validator/CheckImplementations.py
+++ src/sql_guard/validator/CheckImplementations.py
@@ -4,34 +4,52 @@ from typing import Dict
 @CheckRegistry.register("is_integer")
 class IsIntegerCheck(BaseCheck):
     def to_sql(self, column: str, params: Dict, dialect: str) -> str:
-        return f"SAFE_CAST({column} AS INT) IS NULL"
+        return f"SAFE_CAST({column} AS INT) IS NOT NULL"
 
 @CheckRegistry.register("is_string")
 class IsStringCheck(BaseCheck):
     def to_sql(self, column: str, params: Dict, dialect: str) -> str:
-        return f"SAFE_CAST({column} AS STRING) IS NULL"
+        return f"SAFE_CAST({column} AS STRING) IS NOT NULL"
 
 @CheckRegistry.register("is_boolean")
 class isBooleanCheck(BaseCheck):
     def to_sql(self, column: str, params: Dict, dialect: str) -> str:
-        return f"SAFE_CAST({column} AS BOOL) IS NULL"
+        return f"SAFE_CAST({column} AS BOOL) IS NOT NULL"
 
 @CheckRegistry.register("is_float")
 class IsFloatCheck(BaseCheck):
     def to_sql(self, column: str, params: Dict, dialect: str) -> str:
-        return f"SAFE_CAST({column} AS FLOAT) IS NULL"
+        return f"SAFE_CAST({column} AS FLOAT) IS NOT NULL"
 
 @CheckRegistry.register("is_date")
 class IsDateCheck(BaseCheck):
     def to_sql(self, column: str, params: Dict, dialect: str) -> str:
-        return f"SAFE_CAST({column} AS DATE) IS NULL"
+        return f"SAFE_CAST({column} AS DATE) IS NOT NULL"
 
 @CheckRegistry.register("is_timestamp")
 class IsTimestampCheck(BaseCheck):
     def to_sql(self, column: str, params: Dict, dialect: str) -> str:
-        return f"SAFE_CAST({column} AS TIMESTAMP) IS NULL"
+        return f"SAFE_CAST({column} AS TIMESTAMP) IS NOT NULL"
 
 @CheckRegistry.register("greater_than")
+class GreaterThanCheck(BaseCheck):
+    def to_sql(self, column: str, params: Dict, dialect: str) -> str:
+        value = params["value"]
+        return f"{column} > {value}"
+
+@CheckRegistry.register("greater_or_equal_than")
+class GreaterThanCheck(BaseCheck):
+    def to_sql(self, column: str, params: Dict, dialect: str) -> str:
+        value = params["value"]
+        return f"{column} >= {value}"
+
+@CheckRegistry.register("less_than")
+class GreaterThanCheck(BaseCheck):
+    def to_sql(self, column: str, params: Dict, dialect: str) -> str:
+        value = params["value"]
+        return f"{column} < {value}"
+
+@CheckRegistry.register("less_or_equal_than")
 class GreaterThanCheck(BaseCheck):
     def to_sql(self, column: str, params: Dict, dialect: str) -> str:
         value = params["value"]
@@ -41,25 +59,25 @@ class GreaterThanCheck(BaseCheck):
 class StartsWithCheck(BaseCheck):
     def to_sql(self, column: str, params: Dict, dialect: str) -> str:
         value = params["value"]
-        return f"{column} NOT LIKE '{value}%'"
+        return f"{column} LIKE '{value}%'"
     
 @CheckRegistry.register("ends_with")
 class EndsWithCheck(BaseCheck):
     def to_sql(self, column: str, params: Dict, dialect: str) -> str:
         value = params["value"]
-        return f"{column} NOT LIKE '%{value}'"
+        return f"{column} LIKE '%{value}'"
     
 @CheckRegistry.register("str_contains")
 class EndsWithCheck(BaseCheck):
     def to_sql(self, column: str, params: Dict, dialect: str) -> str:
         value = params["value"]
-        return f"{column} NOT LIKE '%{value}%'"
+        return f"{column} LIKE '%{value}%'"
     
 @CheckRegistry.register("regex_contains")
 class EndsWithCheck(BaseCheck):
     def to_sql(self, column: str, params: Dict, dialect: str) -> str:
         value = params["value"]
-        return f"NOT REGEXP_CONTAINS({column}, '{value}')"
+        return f"REGEXP_CONTAINS({column}, r'{value}')"
     
 @CheckRegistry.register("between")
 class BetweenCheck(BaseCheck):
@@ -69,3 +87,20 @@ class BetweenCheck(BaseCheck):
 
         return f"{column} BETWEEN {min} AND {max}"
     
+@CheckRegistry.register("is_in")
+class BetweenCheck(BaseCheck):
+    def to_sql(self, column: str, params: Dict, dialect: str) -> str:
+        values = params['value']
+        values = str(values)
+        values = values[1:-1]
+
+        return f"{column} IN ({values})"
+
+@CheckRegistry.register("is_not_in")
+class BetweenCheck(BaseCheck):
+    def to_sql(self, column: str, params: Dict, dialect: str) -> str:
+        values = params['value']
+        values = str(values)
+        values = values[1:-1]
+
+        return f"{column} NOT IN ({values})"
--- src/sql_guard/validator/SQLValidator.py
+++ src/sql_guard/validator/SQLValidator.py
@@ -1,5 +1,4 @@
-from typing import Dict, List, Optional
-from dataclasses import dataclass
+from typing import Dict, List
 from CheckBase import ValidationCheck
 from CheckImplementations import CheckRegistry
 
@@ -10,7 +9,7 @@ class SQLValidator:
         Create a SQLValidator object that can create SQL validations based on data quality rules.
 
         :param data_rules: Dictionary of data quality rules
-        :param sql_dialect: SQL Dialect of SQL query
+        :param sql_dialect: SQL Dialect of SQL query. GoogleSQL is default.
 
         '''
         self.data_rules = data_rules
@@ -21,49 +20,51 @@ class SQLValidator:
         '''Pre-validating schema passed into SQL Validator class'''
         for column, checks in self.data_rules.items():
             if not isinstance(checks, list):
-                raise ValueError(f"Invalid data constraints for column {column}")
+                raise ValueError(f"Invalid data rules for column {column}")
             for check in checks:
                 if not isinstance(check, ValidationCheck):
                     raise ValueError(f"Invalid ValidationCheck for column {column}")
 
 
-    def to_sql(self, from_source: str="your_table") -> str:
+    def to_sql(self, from_source: str="your_table", wrong_values: bool = False) -> str:
+        '''
+        Create a sql query based on data quality rules.
+
+        :param from_source: the origin table of the FROM clause.
+        :param wrong_values: Flag for getting either the wrong or correct data.
+
+        '''
 
         val_conditions = list()
-        invalid_values = list()
 
         for column, rules in self.data_rules.items():
             column_conditions = list()
             for rule in rules:
                 registry_obj = CheckRegistry.get_check(rule.check_name)
                 sql_condition = registry_obj.to_sql(column, rule.params, "GoogleSQl")
-                column_conditions.append(sql_condition)
+                column_conditions.append(f"({sql_condition})")
 
             if column_conditions:
-                val_conditions.append(" OR ".join(column_conditions))
-                invalid_values.append(f"IFNULL(ARRAY_AGG(IF({' OR '.join(column_conditions)}, SAFE_CAST({column} AS STRING), NULL) IGNORE NULLS), [])")
+                val_conditions.append(f"({' AND '.join(column_conditions)})")
+
+        if wrong_values:
+            sql_query = f""""
+
+            SELECT * FROM {from_source}
+            WHERE NOT ({' AND '.join(val_conditions)})
+
+            """
+            return sql_query
 
 
-        sql_query = f'''
-            WITH validation AS (
-                SELECT 
-                    COUNT(*) AS wrong_count,
-                    ARRAY_CONCAT({', '.join(invalid_values)}) AS wrong_values
-                FROM {from_source}
-                WHERE {' OR '.join(val_conditions)}
-            )
-            SELECT 
-                CASE 
-                    WHEN wrong_count > 0 THEN 
-                        'There were ' || wrong_count || ' values wrong'
-                    ELSE 
-                        'All values are valid'
-                END AS validation_result,
-                wrong_values
-            FROM validation;
-            '''
+        sql_query = f""""
+
+            SELECT * FROM {from_source}
+            WHERE {' AND '.join(val_conditions)}
+
+        """
         
-        return sql_query.strip()
+        return sql_query
     
 
 rules = {
@@ -74,29 +75,20 @@ rules = {
             error_msg="Age must be an integer"
         ),
         ValidationCheck(
-            check_name="greater_than",
-            params={"value": 0},
-            error_msg="Age must be positive"
-        ),
-         ValidationCheck(
-            check_name="between",
-            params={"min": 0, 'max': 10}
+            check_name="greater_or_equal_than",
+            params={"value": 18},
+            error_msg="Age must be over 18"
         )
     ],
-    "email": [
+    "name": [
         ValidationCheck(
-            check_name="starts_with",
-            params={"value": "user_"},
-            error_msg="Invalid email format"
-        ),
-        ValidationCheck(
-            check_name="regex_contains",
-            params={"value": "^[^@]+@[^@]+\.[^@]+$"},
-            error_msg="Invalid regex email format"
+            check_name="is_in",
+            params={"value": ['Joseph', 'Mario', 'user_Flash', 'user_Superman']},
+            error_msg="Invalid name format"
         )
     ]
 }
 
 myTableSChema = SQLValidator(rules)
 
-print(myTableSChema.to_sql())
+print(myTableSChema.to_sql(wrong_values=True))
